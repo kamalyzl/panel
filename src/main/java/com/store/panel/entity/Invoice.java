fec7b37e7f2d60ad5e2b176e44db73ffa249f954
kamalyzl <kamaly.kazal@gmail.com>
2025-06-19 04:11:03 -0500

    ci: jar

--- src/main/java/com/store/panel/entity/Invoice.java
+++ src/main/java/com/store/panel/entity/Invoice.java
@@ -2,11 +2,20 @@ package com.store.panel.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
 @Table(name = "invoices")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Invoice {
 
     @Id
